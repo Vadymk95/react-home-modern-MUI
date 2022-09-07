b201d39e9c5511a75718c8f780df5bb689bc5024
Vadym Kononenko <vadymk95@gmail.com>
2022-09-07 12:30:54 +0300

    master/feat/SearchBar/created component with basic structure

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,31 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+import { Search } from '@mui/icons-material';
 
 export const SearchBar: FC = () => {
-  return <div>SearchBar</div>;
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Search..."
+        value=""
+        onChange={() => {}}
+      />
+      <IconButton type="submit" sx={{ p: '10px', color: 'red' }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
 };
